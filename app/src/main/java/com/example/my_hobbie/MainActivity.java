f6d2bd4ae1ab458d2c90ebaf5a608a840c76583a
Exorcist <mtalhead313@gmail.com>
2023-03-30 17:21:47 +0300

    Need to create info page and add some albums

--- app/src/main/java/com/example/my_hobbie/MainActivity.java
+++ app/src/main/java/com/example/my_hobbie/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.my_hobbie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void GoToNewPage(View view){
+        switch(view.getId()){
+            case R.id.Button_Albums:
+                Intent intent= new Intent(this, activity_albumpage.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 
 
