039f164b7acd823d6b0c99cf8bf2a489d70fc3f9
Exorcist <mtalhead313@gmail.com>
2023-04-15 20:54:18 +0300

    DONE
    An application written on android studio about the Linkin Park group. It contains a short description of the band, all studio albums, and information about the band members.

--- app/src/main/java/com/example/my_hobbie/activity_infopage.java
+++ app/src/main/java/com/example/my_hobbie/activity_infopage.java
@@ -3,25 +3,120 @@ package com.example.my_hobbie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.content.res.Resources;
 
 public class activity_infopage extends AppCompatActivity {
+
+    int images[] = {
+            R.drawable.chester,
+            R.drawable.mike,
+            R.drawable.joe,
+            R.drawable.rob,
+            R.drawable.brad,
+            R.drawable.dave
+    };
+
+    ImageButton Next;
+    ImageButton Previous;
+
+    ImageView Band_Photo;
+
+    int i=0;
+
+    TextView Person_Name;
+    TextView Place;
+    TextView Info;
+    TextView Name_Change;
+
+    String[] person_name;
+    String[] place;
+
+    String[] change_name;
+
+    String[] info;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infopage);
+
+        Resources res = getResources();
+        person_name = res.getStringArray(R.array.Band);
+        place = res.getStringArray(R.array.Place);
+        info = res.getStringArray(R.array.Info);
+        change_name = res.getStringArray(R.array.BandInfo);
+
+        Band_Photo = (ImageView) findViewById(R.id.Photo_Person);
+        Person_Name = (TextView) findViewById(R.id.Name);
+        Place = (TextView) findViewById(R.id.Place);
+        Info = (TextView) findViewById(R.id.Info);
+        Name_Change = (TextView) findViewById(R.id.InfoAbout);
+
+        Next =(ImageButton) findViewById(R.id.imageButtonNext2);
+        Previous = (ImageButton) findViewById(R.id.imageButtonPrevious2);
+
+        String info_about = "Info about ";
+
+        Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i++;
+                if(i>5)
+                    i=0;
+                Band_Photo.setImageResource(images[i]);
+                Person_Name.setText(person_name[i]);
+                Place.setText(place[i]);
+                Info.setText(info[i]);
+                Name_Change.setText(info_about + "" + change_name[i]);
+            }
+        });
+
+        Previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i--;
+                if(i<0)
+                    i=5;
+                Band_Photo.setImageResource(images[i]);
+                Person_Name.setText(person_name[i]);
+                Place.setText(place[i]);
+                Info.setText(info[i]);
+                Name_Change.setText(info_about + "" + change_name[i]);
+            }
+        });
+
+//        Previous.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                i--;
+//                if(i<0)
+//                    i=6;
+//                Album_Cover.setImageResource(images[i]);
+//                Album_Name.setText(album_names[i]);
+//                Genre.setText(genres[i]);
+//
+//
+//            }
+//        });
+//    }
+
     }
 
     public void GoToNewPage(View view){
         switch(view.getId()){
-            case R.id.main_page:
-                Intent intent1= new Intent(this, activity_albumpage.class);
+            case R.id.Main_Page_Button:
+                Intent intent1= new Intent(this, MainActivity.class);
                 startActivity(intent1);
                 break;
-            case R.id.Button_Albums:
-                Intent intent2= new Intent(this, activity_infopage.class);
+            case R.id.Albums_Button:
+                Intent intent2= new Intent(this, activity_albumpage.class);
                 startActivity(intent2);
             default:
                 break;
